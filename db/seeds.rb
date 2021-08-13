931001d56cce9f3d8b6570a74d0ba7868e81d80d
bwesterg <benton.westergaard@gmail.com>
2021-08-13 21:27:06 +0200

    migrated and seeded candy database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Candy.destroy_all
+
+Candy.create([
+    {
+        name: "Skittles",
+        origin: "United States",
+        image: "https://toppng.com/uploads/preview/skittles-png-11552942500yrfnlonfye.png",
+        price: "1.25"
+    },
+    {
+        name: "Snickers",
+        origin: "United States",
+        image: "https://toppng.com/uploads/preview/snickers-png-11552942934fzcniccrc3.png",
+        price: "1.19"
+    },
+    {
+        name: "Maltasers",
+        origin: "United Kingdom",
+        image: "https://banner2.cleanpng.com/20180703/qjr/kisspng-maltesers-twix-candy-chocolate-bounty-cineplex-odeon-corporation-5b3b09d47fb5a6.2131967715305957965231.jpg",
+        price: "1.67"
+    },
+    {
+        name: "Ovomaltine",
+        origin: "Switzerland",
+        image: "https://e7.pngegg.com/pngimages/1020/790/png-clipart-ovaltine-cream-chocolate-bar-hot-chocolate-swiss-cuisine-ovomaltine-cream-food-thumbnail.png",
+        price: "3.00"
+    },
+])
